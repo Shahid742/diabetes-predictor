86740fafe2eee71d1ab5b2d6fdd43d468743f9ee
Shahid <your-email@example.com>
2025-03-08 22:39:23 +0530

    final

--- user_management/settings.py
+++ user_management/settings.py
@@ -32,8 +32,9 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['diabetes-predictor-production.up.railway.app']
 
+CSRF_TRUSTED_ORIGINS = ['https://diabetes-predictor-production.up.railway.app']
 
 # Application definition
 
